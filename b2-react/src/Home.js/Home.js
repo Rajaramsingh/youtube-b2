441db2b1a458a976fcbf6b4f0fbeca6b9cdb8cf1
Rajaramsingh <rajaramsingh.cse2023@ritroorkee.com>
2023-11-06 11:22:35 +0530

    youtube

---
+++ b2-react/src/Home.js/Home.js
@@ -0,0 +1,183 @@
+function Home(){
+    return (
+        <div>
+           import {AiFillHome} from 'react-icons/ai'
+import {IoIosMusicalNote} from 'react-icons/io'
+import {MdOutlineSubscriptions} from 'react-icons/md'
+import {AiOutlineMenu} from 'react-icons/ai'
+import {BsFillMicFill} from 'react-icons/bs'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineShopping} from 'react-icons/ai'
+import {BsNewspaper} from 'react-icons/bs'
+import {SiYoutubegaming} from 'react-icons/si'
+import {MdOutlineSportsSoccer} from 'react-icons/md'
+import {MdOutlinePodcasts} from 'react-icons/md'
+import {AiOutlineBulb} from 'react-icons/ai'
+function Home() 
+    
+      <div>
+      <div class="navbar">
+          <div id="div1"> 
+            <button id="nav1"><AiOutlineMenu/></button>
+            <div id="nav2"></div>
+          </div>
+          <div id="div2"> 
+            <div class='search'><input class="search-amazon" placeholder="   search"/>
+            <button class='search-icon'><AiOutlineSearch/></button>
+            </div>
+            <button id="nav4"><BsFillMicFill/></button>
+           </div> 
+          <div id="div3"></div>
+         
+        </div>
+     <div id="container2">
+        <div id="sidebar">
+          <button class='logo-button'><AiFillHome/>Home</button>
+          <button class='logo-button'><IoIosMusicalNote/>Music</button>
+          <button class='logo-button'><MdOutlineSubscriptions/>Subcriptions</button><hr/>
+          <button class='logo-button'><AiOutlineShopping/>Shoping</button>
+          <button class='logo-button'><BsNewspaper/>News</button>
+          <button class='logo-button'><SiYoutubegaming/>Gaming</button>
+          <button class='logo-button'><MdOutlineSportsSoccer/>Sports</button>
+          <button class='logo-button'><MdOutlinePodcasts/>Podcasts</button>
+          <button class='logo-button'><AiOutlineBulb/>Learing</button>
+         
+        </div>
+        <div id="main">
+            <div class="item content1 con"></div>
+            <div class="item content2 con"></div>
+            <div class="item content3 con"></div>
+            <div class="item discription">
+              <div class="profile1"></div>
+              <div class="dis1">
+              <h5>10 hours of pure black screen in 4k! (read description)</h5>
+              <p>CandRfun .486K views  5 months ago</p>
+              </div>
+            </div>
+            <div class="item discription">
+            <div class="profile2"></div>
+              <div class="dis2">
+              <h5>Variables in JavaScript | JavaScript Tutorial in Hindi #2</h5>
+              <p>Triggered insaan .12M views  9 months ago</p>
+              </div>
+            </div>
+            <div class="item discription">
+            <div class="profile3"></div>
+              <div class="dis3">
+              <h5>Complete Git and GitHub Tutorial for Beginners..|Github.com</h5>
+              <p>Codewithharry .486K views  5 months ago</p>
+              </div>
+            </div>
+            <div class="item content4 con2"></div>
+            <div class="item content5 con2"></div>
+            <div class="item content6 con2"></div>
+            <div class="item discription">
+            <div class="profile4"></div>
+              <div class="dis4">
+              <h5>10 hours of pure black screen in 4k! (read description)</h5>
+              <p>CandRfun .486K views  5 months ago</p>
+              </div>
+            </div>
+            <div class="item discription">
+            <div class="profile5"></div>
+              <div class="dis5">
+              <h5>10 hours of pure black screen in 4k! (read description)</h5>
+              <p>CandRfun .486K views  5 months ago</p>
+              </div>
+            </div>
+            <div class="item discription">
+            <div class="profile6"></div>
+              <div class="dis6">
+              <h5>10 hours of pure black screen in 4k! (read description)</h5>
+              <p>CandRfun .486K views  5 months ago</p>
+              </div>
+            </div>
+        </div>
+     </div>
+      </div>
+    
+
+
+export default Home;
+
+          
+    
+   
+  
+    
+
+ 
+
+{/* 
+             <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> 
+   
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>College</th>
+            <th>Branch</th>
+
+        </tr>
+        <tr>
+            <td>Ram Bansal</td>
+            <td>admin@explorin.io</td>
+            <td>IIT Roorkee</td>
+            <td>Chemical</td>
+        </tr>
+        <tr><td>Prashant Jindal</td>
+            <td>admin@explorin.io</td>
+            <td>IIT Roorkee</td>
+            <td>Chemical</td>
+        </tr>
+        <tr>
+           <td>Keshav Bidawatka</td>
+           <td>admin@explorin.io</td>
+           <td>IIT Roorkee</td>
+           <td>Civil</td>
+        </tr>
+    </table>
+     */}
+
+    
+        </div>
+    )
+}
+
+
+export default Home;
---
+++ b2-react/src/video.js
@@ -0,0 +1,36 @@
+
+
+
+import React, { useEffect, useState } from 'react'
+
+// function Video() {
+    // const [seconds, setSeconds]=useState(0);
+    // const targetTime =(new Date()).getTime()/1000 +10;                       //+30seconds
+
+    // // when page renders
+    // useEffect(()=>{
+    //     const interval= setInterval(()=>{
+    //         const currentTime=((new Date()).getTime()/1000);
+    //         const remainingTime=targetTime-currentTime;
+
+    //         if(remainingTime >=0){
+    //             setSeconds(remainingTime);
+    //         }
+    //     },1000)
+    // },[])
+//   return (
+    // <div>
+    //     <h2> my video</h2>
+    //     <h4> Time Left:{seconds}</h4>
+      
+    // </div>
+//   )
+// }
+
+export default Video;
+<div>
+    <Header/>
+    <div className=""
+
+    
+</div>
